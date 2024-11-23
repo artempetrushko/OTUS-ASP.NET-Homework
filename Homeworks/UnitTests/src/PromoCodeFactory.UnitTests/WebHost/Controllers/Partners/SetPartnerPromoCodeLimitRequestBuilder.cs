b49291fd9e26847a5decc1615fa0fc667b411908
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2024-11-23 12:23:56 +0500

    Added unit tests for SetPartnerPromoCodeLimitAsync method

---
+++ Homeworks/UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitRequestBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using PromoCodeFactory.WebHost.Models;
+
+namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
+{
+    public class SetPartnerPromoCodeLimitRequestBuilder
+    {
+        private DateTime _endDate;
+        private int _limit;
+
+        public SetPartnerPromoCodeLimitRequestBuilder()
+        {
+            _endDate = DateTime.UtcNow;
+            _limit = 1;
+        }
+
+        public SetPartnerPromoCodeLimitRequestBuilder WithEndDate(DateTime endDate)
+        {
+            _endDate = endDate;
+            return this;
+        }
+
+        public SetPartnerPromoCodeLimitRequestBuilder WithLimit(int limit)
+        {
+            _limit = limit;
+            return this;
+        }
+
+        public SetPartnerPromoCodeLimitRequest Build()
+        {
+            return new SetPartnerPromoCodeLimitRequest()
+            {
+                EndDate = _endDate,
+                Limit = _limit
+            };
+        }
+    }
+}
